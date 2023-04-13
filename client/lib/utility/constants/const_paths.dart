a9942e05ded94c9c069df0bb900a6c66148e740f
moonseok <moonseokp96@gmai.com>
2023-04-13 23:59:11 +0900

    테스트

--- client/lib/utility/constants/const_paths.dart
+++ client/lib/utility/constants/const_paths.dart
@@ -1,16 +1,6 @@
-import 'dart:io';
-
-import 'package:flutter/foundation.dart';
-
 abstract class ConstPaths {
   // asset paths
   static const assets = 'lib/assets';
   static const imageAssets = "$assets/images/bitmaps/";
   static const svgAssets = "$assets/images/vectors/";
-
-  // server paths
-  static const _productionUrl = '43.200.187.166:7300';
-  static final _developUrl =
-      Platform.isAndroid ? '192.168.0.2' : 'localhost:7300';
-  static final baseUrl = kReleaseMode ? _productionUrl : _developUrl;
 }
